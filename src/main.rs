cad538fda64573a4f66ee8352847254faa26a555
Daniel Brai <braidaniel2001@gmail.com>
2023-06-22 23:45:33 +0100

    dev(app): done with implementation of podchecker

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
-use axum::{routing::get, Router};
-
-async fn hello_world() -> &'static str {
-    "Hello, world!"
-}
+use podchecker::Router;
 
 #[shuttle_runtime::main]
 async fn axum() -> shuttle_axum::ShuttleAxum {
-    let router = Router::new().route("/", get(hello_world));
+    let podcasts = read_podcasts_from_xml("https://workingdraft.de/feed/").await?;
+    let app_state = Arc::new(podcasts);
+    let router = Router::new()
+        .route("/", get(podchecker::root()))
+        .route("/:id", get(podchecker::podcast()))
+        .with_state(app_state);
 
     Ok(router.into())
 }
